c96e74e1e03adeb2a66362190b3e549242bf570f
Federico <brunetti.federico.13@gmail.com>
2025-05-20 17:15:39 +0200

    convertito componente booklist in componente function

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,45 +1,47 @@
-import { Component } from "react";
+import { useState } from "react";
 import { Col, Container, FormControl, Row } from "react-bootstrap";
 import SingleBook from "./SingleBook";
 import CommentArea from "./CommentArea";
 
-class BookList extends Component {
-  state = {
+const BookList = function ({ genre }) {
+  /* state = {
     searchQuery: "",
     selectedBookAsin: "",
-  };
-  changeAsin = (asin) => this.setState({ selectedBookAsin: asin });
+  }; */
+  const [searchQuery, setSearchQuery] = useState("");
 
-  render() {
-    return (
-      <Container fluid>
-        <h2>Libri Disponibili</h2>
+  const [selectedBookAsin, setSelectedBookAsin] = useState("");
 
-        <FormControl
-          className="w-50 my-4"
-          type="text"
-          placeholder="Cerca un titolo di un libro"
-          value={this.state.searchQuery}
-          onChange={(e) => this.setState({ searchQuery: e.target.value })}
-        />
-        <Row>
-          <Col md={8}>
-            {" "}
-            <Row xs={1} sm={2} md={3} lg={4} xxl={6} className="g-*">
-              {this.props.genre
-                .filter((book) => book.title.toLowerCase().includes(this.state.searchQuery.toLowerCase()))
-                .map((book) => (
-                  <SingleBook key={book.asin} book={book} changeAsin={this.changeAsin} />
-                ))}
-            </Row>{" "}
-          </Col>
-          <Col md={4}>
-            <CommentArea id={this.state.selectedBookAsin} />
-          </Col>{" "}
-        </Row>
-      </Container>
-    );
-  }
-}
+  const changeAsin = (asin) => setSelectedBookAsin(asin);
+
+  return (
+    <Container fluid>
+      <h2>Libri Disponibili</h2>
+
+      <FormControl
+        className="w-50 my-4"
+        type="text"
+        placeholder="Cerca un titolo di un libro"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
+      <Row>
+        <Col md={8}>
+          {" "}
+          <Row xs={1} sm={2} md={3} lg={4} xxl={6} className="g-*">
+            {genre
+              .filter((book) => book.title.toLowerCase().includes(searchQuery.toLowerCase()))
+              .map((book) => (
+                <SingleBook key={book.asin} book={book} changeAsin={changeAsin} />
+              ))}
+          </Row>{" "}
+        </Col>
+        <Col md={4}>
+          <CommentArea id={selectedBookAsin} />
+        </Col>{" "}
+      </Row>
+    </Container>
+  );
+};
 
 export default BookList;
